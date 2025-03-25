1e88a66256ce006470b1233fd68bd0a00e43df17
sdwerswwww <reygeneva39@gmail.com>
2025-03-25 14:49:08 +0300

    Add files via upload

---
+++ navig.js
@@ -0,0 +1,22 @@
+
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+galleryItems.forEach(item => {
+    observer.observe(item);
+});
+
